0d4d5e4c319d00958f9f2c1d260296b439256ddd
Tomasz Czerwinski <tomasz.czerwinski.1997@gmail.com>
2021-08-11 01:14:09 +0200

    Fixed App component

--- src/components/App.js
+++ src/components/App.js
@@ -2,15 +2,13 @@ import { connect } from "react-redux";
 
 import { incrementValue, decrementValue } from "../actions";
 
-const App = ({count, incrementValue, decrementValue}) => {
-  return (
-    <div>
-      <button onClick={incrementValue}>+</button>
-      {count}
-      <button onClick={decrementValue}>-</button>
-    </div>
-  );
-}
+const App = ({count, incrementValue, decrementValue}) => (
+  <div>
+    <button onClick={incrementValue}>+</button>
+    {count}
+    <button onClick={decrementValue}>-</button>
+  </div>
+);
 
 const mapStateToProps = state => {
   return { count: state };
